0fe2c9eac783ddfef40ce0673478f36616ec81fb
Lucca-Lopes <luccavlopes@hotmail.com>
2023-02-01 17:01:16 -0300

    Projeto Criado

---
+++ NanoAPI/NanoAPI/NanoAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoAPIApp.swift
+//  NanoAPI
+//
+//  Created by Lucca Lopes on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
