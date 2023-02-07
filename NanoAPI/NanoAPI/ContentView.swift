fb9e179073fe941a89593d15ff07461a95f945e9
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2023-02-07 14:35:50 -0300

    chave ocultada mais ainda

--- NanoAPI/NanoAPI/ContentView.swift
+++ NanoAPI/NanoAPI/ContentView.swift
@@ -11,18 +11,30 @@ struct ContentView: View {
     var body: some View {
         VStack {
             if let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String{
-                Image(systemName: "key.fill")
-                    .imageScale(.large)
-                    .foregroundColor(.gray)
-                Text(apiKey)
+                Text("apiKey")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.accentColor)
+                HStack{
+                    Image(systemName: "key.fill")
+                        .imageScale(.large)
+                        .foregroundColor(.gray)
+                    Text(apiKey)
+                        .foregroundColor(.pink)
+                        .bold()
+                        .font(.title)
+                    Image(systemName: "key.fill")
+                        .imageScale(.large)
+                        .foregroundColor(.gray)
+                }
             }
         }
         .padding()
     }
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
