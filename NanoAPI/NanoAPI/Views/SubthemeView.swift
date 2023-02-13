1d5bb680b65683d1766e89266c07099415473023
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2023-02-13 15:00:13 -0300

    front da tela de subtemas finalizado(Eu acho)

--- NanoAPI/NanoAPI/Views/SubthemeView.swift
+++ NanoAPI/NanoAPI/Views/SubthemeView.swift
@@ -23,23 +23,23 @@ struct SubthemeView: View {
     
     var body: some View {
         Divider()
-        //        Spacer()
-        //        Spacer()
-//        Form{
-//            Text("Subtemas")
-//            Text("Ano")
-//            Text("Última Atualização")
-//        }
 
         ScrollView {
             LazyVStack {
                 Text("""
-                     Subtemas
-                     Ano
-                     Última Atualização
+                     Subtemas: \(tema.subthemeCount)
+                     Ano: \(String(tema.yearFrom))
+                     Última Atualização: \(String(tema.yearTo))
                      """)
+                .lineSpacing(3)
+                .padding()
+                .font(.system(size: 13))
                 .frame(width: screenWidth * 0.8, alignment: .leading)
-                .background(RoundedRectangle(cornerRadius: 5) .fill(.gray) .frame(width: screenWidth * 0.8))
+                .background{
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(Color(uiColor: .systemGray5))
+                        .frame(width: screenWidth * 0.8)
+                }
                 
                 Spacer(minLength: 30)
                 
